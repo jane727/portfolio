08d9ab05ed2213d2e931b26529d08442e85955cf
jane727 <bykim125@gmail.com>
2023-08-24 11:15:46 +0900

    intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import { BsArrowUpRight } from 'react-icons/bs';
+import { ReactComponent as Ghost } from '../assets/imgs/ghost.svg';
 
-const Intro = () => <div>Intro</div>;
-
-export default Intro;
+export default function Intro() {
+	return (
+		<section id='intro'>
+			<div className='intro__inner'>
+				<div className='intro__folder'>
+					<span className='intro__folder__subtitle'>Welcome to my portfolio</span>
+					<span className='intro__folder__title'>
+						To Infinite
+						<br />
+						and beyond !
+					</span>
+					<div className='intro__folder__bottom'>
+						<span>Take a look at my works.</span>
+						<a href='#projects'>
+							view <BsArrowUpRight />
+						</a>
+					</div>
+				</div>
+				<div className='intro__ghost'>
+					<Ghost />
+				</div>
+			</div>
+		</section>
+	);
+}
--- src/pages/HomeView.jsx
+++ src/pages/HomeView.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Header from '../components/Header';
 import Main from '../components/Main';
-import Intro from '../components/Intro';
 import About from '../components/About';
 import Projects from '../components/Projects';
 import Footer from '../components/Footer';
 import Contact from '../components/Contact';
+import Intro from '../components/Intro';
 
 export default function HomeView() {
 	return (
